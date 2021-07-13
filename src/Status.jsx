c49eac06d42120434e35581e44307bfd91a4348d
viktoriaD29 <77213795+viktoriaD29@users.noreply.github.com>
2021-07-13 16:11:34 +0300

    Update Status.jsx

--- src/Status.jsx
+++ src/Status.jsx
@@ -2,36 +2,9 @@ import React, { Component } from 'react';
 import Online from './Online.jsx';
 import Offline from './Offline.jsx';
 
-class Status extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isOnline: false,
-    };
-  }
-
-  handleOnline = () => {
-    // console.log(22);
-    // this.setState = {
-    //   isOnline: true,
-    // };
-    this.setState((state) => ({ ...state, isOnline: true }));
-    
-  };
-
-  render() {
-    console.log(this.state.isOnline)
-    return (
-      <div>
-        {this.state.isOnline ? (
-          <Online />
-        ) : (
-          <Offline onOnline={this.handleOnline} />
-        )}
-      </div>
-    );
-  }
-}
+const Status = (props) => {
+  const { isOnline } = props;
+  return isOnline ? <Online /> : <Offline onOnline={this.handleOnline} />;
+};
 
 export default Status;
