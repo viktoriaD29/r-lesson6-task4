69b4dabebe2c863d61448e70d0f519bae5408105
viktoriaD29 <77213795+viktoriaD29@users.noreply.github.com>
2021-07-13 16:20:23 +0300

    Update Offline.jsx

--- src/Offline.jsx
+++ src/Offline.jsx
@@ -4,7 +4,7 @@ const Offline = (props) => {
   return (
     <div className="status">
       <span className="status__text">Offline</span>
-      <button className="status__btn" onClick={props.onOnline}>
+      <button className="status__btn">
         Recconect
       </button>
     </div>
